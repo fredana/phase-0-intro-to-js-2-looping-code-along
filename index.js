0ba57ea6b75be9aabaefccce74de69fe929c3b07
freda <ndyfreda@gmail.com>
2023-04-06 21:57:13 -0400

    looping code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array){   
+    array = ["Guadalupe", "Ollie", "Aki"];
+    let message = [];
+    for (let i=0; i<array.length; i++){       
+        message.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);       
+    }
+    return message;
+}
+ 
+function countDown(num){
+    num = 11;
+    while (num>0) {
+        console.log(num=num-1);
+    }
+}
+
